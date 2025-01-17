dc17fd88def15e4f44677825de55a21dee7bf2cb
Rqmba <ramblack45@gmail.com>
2025-01-17 18:50:32 +0100

    Create state for tasks

--- src/App.js
+++ src/App.js
@@ -1,9 +1,33 @@
+import { useState } from "react"
 import Header from "./components/Header";
+import Tasks from "./components/Tasks";
 
 function App() {
+  const [tasks, setTasks] = useState([
+    {
+        id: 1,
+        text: 'Rendez vous medecin',
+        day: '5 fevrier à 10H30',
+        reminder: true,
+    },
+    {
+        id: 2,
+        text: 'Boxe',
+        day: '7 fevrier à 19H30',
+        reminder: true,
+    },
+    {
+        id: 3,
+        text: 'Diner pro',
+        day: '14 fevrier à 20H30',
+        reminder: false,
+    },
+])
+
   return (
     <div className="container">
       <Header />
+      <Tasks tasks={tasks} />
     </div>
   );
 }
--- src/components/Header.js
+++ src/components/Header.js
@@ -1,10 +1,18 @@
 import Button from "./Button";
 
 const Header = ({ title = 'Suivi de taches' }) => {
+  const onClick = (e) => {
+    console.log('Click')
+  }
+
   return (
     <header className="header">
         <h1>{title}</h1>
-        <Button color='green' text='Ajouter' />
+        <Button 
+        color='green' 
+        text='Ajouter'
+        onClick={onClick} 
+        />
     </header>
   )
 }
